1b1c284e2ec2b9353dcfd7c5af51c0db6962314d
tchcfhvgd <99186628+tchcfhvgd@users.noreply.github.com>
2024-01-20 10:31:39 +0800

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -75,11 +75,6 @@ class Main extends Sprite
 		ClientPrefs.loadDefaultKeys();
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
 
-		#if debug
-		FlxG.console.registerClass(FlxBasic);
-		#end
-
-		#if !mobile
 		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
 		Lib.current.stage.align = "tl";
@@ -87,7 +82,6 @@ class Main extends Sprite
 		if(fpsVar != null) {
 			fpsVar.visible = ClientPrefs.showFPS;
 		}
-		#end
 
 		#if html5
 		FlxG.autoPause = false;
